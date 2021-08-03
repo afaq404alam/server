a43d8ec3006138aae72fab1b3c7497364eccdaa6
afaq404alam <afaq404alam@gmail.com>
2021-08-03 23:26:44 +0200

    Stream the response directly to TcpStream instead of double allocating objects on heap.

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::{io::Read, io::Write, net::TcpListener};
 
@@ -23,12 +23,22 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
-                                    write!(stream, "HTTP/1.1 404 Not Found");
+                                    Response::new(
+                                        StatusCode::NotFound,
+                                        Some("<h1> IT WORKS!!!</h1>".to_string()),
+                                    )
                                 }
-                                Err(e) => println!("Failed to parse a request: {}", e),
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
